5228d492ab3cbefec79551187be08b41c47e4ec8
SkylarYHU <skylarhyn@gmail.com>
2024-11-18 11:38:00 +0000

    Update settings

--- poll/settings.py
+++ poll/settings.py
@@ -144,26 +144,45 @@ USE_I18N = True
 USE_TZ = True
 
 
-if DEBUG:
-    STATIC_URL = '/static/'
-    MEDIA_URL = '/media/'
-    STATICFILES_DIRS = [BASE_DIR / 'static']
-    MEDIA_ROOT = BASE_DIR / 'media'
-else:
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', None)
+# if DEBUG:
+#     STATIC_URL = '/static/'
+#     MEDIA_URL = '/media/'
+#     STATICFILES_DIRS = [BASE_DIR / 'static']
+#     MEDIA_ROOT = BASE_DIR / 'media'
+# else:
+#     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', None)
 
-    print("AWS_STORAGE_BUCKET_NAME:", AWS_STORAGE_BUCKET_NAME)
-    
-    if not AWS_STORAGE_BUCKET_NAME:
-        raise ValueError("AWS_STORAGE_BUCKET_NAME must be set in production")
-    
-    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
-    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
-    STATIC_ROOT = BASE_DIR / "staticfiles"
+#     print("AWS_STORAGE_BUCKET_NAME:", AWS_STORAGE_BUCKET_NAME)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+#     if not AWS_STORAGE_BUCKET_NAME:
+#         raise ValueError("AWS_STORAGE_BUCKET_NAME must be set in production")
+    
+#     STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
+#     MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
+#     STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'polls_list'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_STORAGE_BUCKET_NAME = 'vocalize-poll'
+AWS_S3_REGION_NAME = 'eu-north-1'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+AWS_QUERYSTRING_AUTH = False
+
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://vocalize-poll.up.railway.app']
