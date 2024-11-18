e79c091925d3c7924532e452f91c889c36170991
SkylarYHU <skylarhyn@gmail.com>
2024-11-18 11:10:21 +0000

    Create start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn mysite.wsgi:application --bind 0.0.0.0:8080
+gunicorn poll.wsgi:application --bind 0.0.0.0:8080
